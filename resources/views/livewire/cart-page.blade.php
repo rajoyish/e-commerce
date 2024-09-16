d35ae9751a89f62e76ca088b99d351c3a8dd9602
Rajesh Budhathoki <rajoyish@gmail.com>
2024-09-16 08:02:55 +0545

    19 Login & Register functionality

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -85,7 +85,8 @@
                         <span class="font-semibold">$21.98</span>
                     </div>
                     @if($cart_items)
-                        <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+                        <a href="/checkout"
+                           class="bg-blue-500 text-white block text-center py-2 px-4 rounded-lg mt-4 w-full">Checkout</a>
                     @endif
                 </div>
             </div>
