b09a53ea06443ef6883b513790b16b709a8b6b58
Rajesh Budhathoki <rajoyish@gmail.com>
2024-09-17 08:28:36 +0545

    20 Password Reset

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -15,9 +15,18 @@
                         </p>
                     </div>
 
+
                     <hr class="my-5 border-slate-300">
 
                     <!-- Form -->
+                    @if(session('error'))
+                        <div
+                            class="mt-2 mb-4 bg-red-100 border border-red-200 text-sm text-red-800 rounded-lg p-4 dark:bg-red-800/10 dark:border-red-900 dark:text-red-500"
+                            role="alert" tabindex="-1" aria-labelledby="hs-soft-color-danger-label">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+
                     <form wire:submit.prevent="save">
                         <div class="grid gap-y-4">
                             <!-- Form Group -->
